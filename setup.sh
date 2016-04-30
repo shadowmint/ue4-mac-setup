ff0d3d216dd304b4cd6ad95df72236dbf972f691
Douglas Linder <linderd@iinet.net.au>
2016-04-30 13:52:53 +0800

    new version, still broken on mac

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
-RELEASE=4.11.0-release
+RELEASE=4.11.2-release
 git clone git@github.com:EpicGames/UnrealEngine.git --recursive --depth 1 --branch $RELEASE
 cd UnrealEngine
 sh ./Setup.sh
 sh ./GenerateProjectFiles.sh
-xcodebuild -workspace UE4.xcworkspace -scheme UE4
+open UE4.xcworkspace
